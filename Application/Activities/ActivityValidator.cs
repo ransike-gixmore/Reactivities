7fbb5251d23ee7d866ad2552267e5e20b8e8c6a0
ransike-gixmore <87597044+ransike-gixmore@users.noreply.github.com>
2021-09-09 20:35:26 +0530

    End of Section 10

---
+++ Application/Activities/ActivityValidator.cs
@@ -0,0 +1,19 @@
+using Domain;
+using FluentValidation;
+
+namespace Application.Activities
+{
+    public class ActivityValidator : AbstractValidator<Activity>
+    {
+        public ActivityValidator()
+        {
+            RuleFor(X => X.Title).NotEmpty();
+            RuleFor(X => X.Description).NotEmpty();
+            RuleFor(X => X.Date).NotEmpty();
+            RuleFor(X => X.Category).NotEmpty();
+            RuleFor(X => X.City).NotEmpty();
+            RuleFor(X => X.Venue).NotEmpty();
+        }
+        
+    }
+}
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,6 +1,8 @@
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -8,12 +10,20 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -21,15 +31,17 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Activities.Add(request.Activity);
                 //Not adding Data to DB, just adding data to the memory only. Thus not accessing the DB at the moment.
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
                 //Above line will save changes to the DB.
 
-                return Unit.Value;
+                if (!result) return Result<Unit>.Failure("Failed to create activity");
+
+                return Result<Unit>.Success(Unit.Value);
                 //Unit is just an object that the mediator provides but doesn't really have any actual value, like returning nothing
                 //But it will tell the API that the request is finished and it's gonna move on
                 //When using mediator, it will be waiting for task to complete, and it will tell the api that our task is complete.
